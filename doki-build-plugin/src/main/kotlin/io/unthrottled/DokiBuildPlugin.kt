a383bff4043d7bb844b200b21767ca7ce2e5a04f
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2025-11-15 13:31:42 -0600

    code cleanup

--- doki-build-plugin/src/main/kotlin/io/unthrottled/DokiBuildPlugin.kt
+++ doki-build-plugin/src/main/kotlin/io/unthrottled/DokiBuildPlugin.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.register
 
-public class DokiBuildPlugin: Plugin<Project> {
+class DokiBuildPlugin: Plugin<Project> {
   override fun apply(project: Project) {
     project.tasks.register<BuildThemesTask>("buildThemes") {
       group = "build"
